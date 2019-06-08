5f3df1104d6d9d98c5f3227985c288025f03dc10
Rp7rf <westhouse51@gmail.com>
2019-06-08 18:12:41 +0900

    finished hackathon

--- app/controllers/materials_controller.rb
+++ app/controllers/materials_controller.rb
@@ -1,6 +1,6 @@
 class MaterialsController < ApplicationController
   def show
-    @material = Material.find_by(params.permit(:name))
+    @material = Material.find_by(name: params[:id])
     render 'show', formats: 'json'
   end
 end
--- app/controllers/photos_controller.rb
+++ app/controllers/photos_controller.rb
@@ -4,16 +4,30 @@ protect_from_forgery :except => [:create]
   def create
     client.authorization :Key, '734f2eb1891744b9b458fe33b120cb4d'
     client.headers['Authorization']
-    client.post '/https://api.clarifai.com/v2/models/aaa03c23b3724a16a56b629203edc62c/outputs', { inputs: { data: { image: url } } }
+    image = params[:image]
+    if image.present?
+      prefix = image[/(image|application)(\/.*)(?=\;)/]
+      # type = prefix.sub(/(image|application)(\/)/, '')
+      data = Base64.decode64(image.sub(/data:#{prefix};base64,/, ''))
+      filename = "#{Time.zone.now.strftime('%Y%m%d%H%M%S%L')}.#{'neko'}.jpeg"
+      File.open("#{Rails.root}/public/#{filename}", 'wb') do |f|
+        f.write(data)
+      end
+      # eyecatch.detach if eyecatch.attached?
+      # eyecatch.attach(io: File.open("#{Rails.root}/tmp/#{filename}"), filename: filename)
+      FileUtils.rm("#{Rails.root}/public/#{filename}")
+
+      binding.pry
+    end
   end
 
   def show
-    binding.pry
+    # binding.pry
   end
 
   private
 
   def client
-    @client ||= Faraday::Connection.new 'http://sushi.com'
+    @client ||= Faraday::Connection.new 'https://api.clarifai.com/v2/models/aaa03c23b3724a16a56b629203edc62c/outputs'
   end
 end
